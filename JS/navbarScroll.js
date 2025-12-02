43e4a7821d3aa3828b1a03796c11783cf3a89bae
Exequiel Miguez <miguezexequiel@hotmail.com>
2025-12-02 12:06:03 -0300

    pagina medicoss

---
+++ JS/navbarScroll.js
@@ -0,0 +1,8 @@
+﻿window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scroll');
+    } else {
+        navbar.classList.remove('navbar-scroll');
+    }
+});
